2a86a69d49f50db97c3641173b0539caa8d7a6e4
zb0o1 <mrzb900@gmail.com>
2026-02-17 21:35:32 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8428564876:AAFBZb9i4n5O4nYyDkXYzCe1ikt1qK6E_KY"
+OWNER_ID = 7503095133 # Replace with your Telegram ID
+SUPPORT_URL = "https://t.me/+4h4qBT0onr4zMWRl"
+UPDATES_URL = "https://t.me/+CYeki1NeFyIzNzFl"
+BOT_USERNAME = "@nancywelcome_bot"
